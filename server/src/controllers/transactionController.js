0c2881a507c8e2e14084f7b334bf36d741190eb9
Shubhi <joshigarima906@gmail.com>
2026-03-15 18:05:45 +0530

    feat: add idempotency validation controller for safe transaction processing

--- server/src/controllers/transactionController.js
+++ server/src/controllers/transactionController.js
@@ -19,4 +19,37 @@ async function createTransaction(req, res) {
       message: "Invalid fromAccount or toAccount",
     });
   }
+
+  const isTransactionAlreadyExists = await transactionModel.findOne({
+    idempotencyKey: idempotencyKey,
+  });
+
+  if (isTransactionAlreadyExists) {
+    if (isTransactionAlreadyExists.status === "COMPLETED") {
+      res.status(200).json({
+        message: "Transaction already processed",
+        transaction: isTransactionAlreadyExists,
+      });
+    }
+
+    if (isTransactionAlreadyExists.status === "PENDING") {
+      res.status(200).json({
+        message: "Transaction is still processing",
+      });
+    }
+
+    if (isTransactionAlreadyExists.status === "FAILED") {
+      res.status(500).json({
+        message: "Transaction processing failed, please retry",
+        transaction: isTransactionAlreadyExists,
+      });
+    }
+
+    if (isTransactionAlreadyExists.status === "REVERSED") {
+      res.status(500).json({
+        message: "Transaction was reversed, please retry",
+        transaction:isTransactionAlreadyExists
+      });
+    }
+  }
 }
